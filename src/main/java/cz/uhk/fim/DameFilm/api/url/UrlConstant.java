a78de32a4160154f82883a77aefef36632abf02b
rehak.vit.hk@gmail.com <uubbaall2413>
2022-05-14 14:16:49 +0200

    Complete

--- src/main/java/cz/uhk/fim/DameFilm/api/url/UrlConstant.java
+++ src/main/java/cz/uhk/fim/DameFilm/api/url/UrlConstant.java
@@ -1,9 +1,9 @@
 package cz.uhk.fim.DameFilm.api.url;
 
 public class UrlConstant {
-    public static final String AUTH = "/auth";
-    public static final String MOVIE = "/movie";
-    public static final String Comment = "/comment";
-    public static final String Rating = "/rating";
+    public static final String AUTH = "api/auth";
+    public static final String MOVIE = "api/movie";
+    public static final String Comment = "api/comment";
+    public static final String Rating = "api/rating";
 
 }
